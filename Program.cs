1da12567f7bafbcff81e018e79b2fa35b649d195
Dan Actor <danltactor@gmail.com>
2018-03-07 16:33:03 -0700

    git init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace C_TodoList
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+	}
+	public class Todo 
+	{ 
+		public int TodoId { get; set; } 
+		public string Name { get; set; } 
+		public string Body { get; set; } 
+
+		public static List<Todo> Todos { get; set; } 
+	} 
+}
